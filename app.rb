6edf4a2ba7ac4a2b2dae1eaa172cabeb695b50ae
Rafael Henrique Manoel <rafael.manoel@gmail.com>
2010-03-31 10:59:03 -0300

    adding more functionality

--- app.rb
+++ app.rb
@@ -11,13 +11,12 @@ require 'yql'
 
   get '/search' do
 		ymap = YMap.new
-		coords = ymap.get_coords_for_address params[:address]
 
-    content_type 'application/xml', :charset => 'utf-8'
-    xml = <<EOF
-<result><Address><address_latitude>#{coords[:latitude]}</address_latitude><address_longitude>#{coords[:longitude]}</address_longitude></Address><delegacy><delegacy_name>14º DP</delegacy_name><delegacy_latitude>-23.561866</delegacy_latitude><delegacy_longitude>-46.691292</delegacy_longitude><crimes><thieves>6.274</thieves><robbery>2.875</robbery><kidnapping>-</kidnapping><extortion>2</extortion><swindler>748</swindler><others>395</others><total>10.294</total></crimes></delegacy><percentages><p_thieves>3.0%</p_thieves><p_robbery>1.0%</p_robbery><p_kiddnapping>-</p_kiddnapping><p_extortion>0.0%</p_extortion><p_swindler>4.0%</p_swindler><p_others>2.0%</p_others><p_total>2.0%</p_total></percentages></result>
-EOF
-    xml
+		coords = ymap.get_coords_for_address params[:address]
+		crimes = Yql.new.get_criminal_info("14")
+		
+		content_type 'application/xml', :charset => 'utf-8'
+		build_xml :coords => coords, :crime_info => crimes
   end
   
   get '/map' do
@@ -34,12 +33,47 @@ EOF
  	end	
   
 	get '/info' do
+
+		ymap = YMap.new
+		coords = ymap.get_coords_for_address "Rua ofélia, 200"
+		crimes = Yql.new.get_criminal_info("14")
 		
-		Yql.new.get_criminal_info "26º"
-		
-		"Okie"
+		puts crimes
+
+		build_xml :coords => coords, :crime_info => crimes
+
 	end  
   
-  
+  private
+  def build_xml att={}
+  	coords = att[:coords]
+  	crimes = att[:crime_info]
+  	
+xml=<<EOF		
+<result>
+	<Address>
+		<address_latitude>#{coords[:latitude]}</address_latitude>
+		<address_longitude>#{coords[:longitude]}</address_longitude>
+	</Address>
+	<delegacy>
+		<delegacy_name>#{crimes[:delegacy_name]}</delegacy_name>
+		<delegacy_latitude>-23.561866</delegacy_latitude>
+		<delegacy_longitude>-46.691292</delegacy_longitude>
+		<crimes>
+			<thieves>#{crimes[:thieves]}</thieves>
+			<robbery>#{crimes[:robbery]}</robbery>
+			<kidnapping>#{crimes[:kidnapping]}</kidnapping>
+			<extortion>#{crimes[:extortion]}</extortion>
+			<swindler>#{crimes[:swindler]}</swindler>
+			<others>#{crimes[:others]}</others>
+			<total>#{crimes[:total]}</total>
+		</crimes>
+	</delegacy>
+	<percentages><p_thieves>3.0%</p_thieves><p_robbery>1.0%</p_robbery><p_kiddnapping>-</p_kiddnapping><p_extortion>0.0%</p_extortion><p_swindler>4.0%</p_swindler><p_others>2.0%</p_others><p_total>2.0%</p_total></percentages>
+</result>
+EOF
+	
+		xml
+ 	end
   
   
--- yql.rb
+++ yql.rb
@@ -1,30 +1,28 @@
 class Yql
 	
-	BASE_YQL_URL = "http://query.yahooapis.com/v1/public/yql?format=xml&q=use+%27http://violenciasp.appspot.com/crimescontrapatrimonio.xml%27+as+indice_violencia;select+*+from+indice_violencia";
+#	BASE_YQL_URL = 
+	#=     %20'14%C2%BA%20DP'%20
+	#"http://query.yahooapis.com/v1/public/yql?format=xml&q=use+%27http://violenciasp.appspot.com/crimescontrapatrimonio.xml%27+as+indice_violencia;select+*+from+indice_violencia";
+	
+	
+	DP ={"14" => [-23.561866,	-46.691292]} 
 	
 	def get_criminal_info nome
 		
-		url = BASE_YQL_URL + URI.escape(" where nome = #{nome} ")
+		url = "http://query.yahooapis.com/v1/public/yql?q=use%20'http%3A%2F%2Fviolenciasp.appspot.com%2Fcrimescontrapatrimonio.xml'%20as%20indice_violencia%3B%20select%20*%20from%20indice_violencia%20where%20nome%20%3D%20'#{nome}%C2%BA%20DP'%20"
 
-		puts url
-
-xml = AppEngine::URLFetch::HTTP.get(URI.parse(url))
-		puts xml
+		xml = AppEngine::URLFetch::HTTP.get(URI.parse(url))
 
 		doc =	Hpricot(xml) 	
 		
-		puts doc
-		
-		puts doc.at("delegacy").inner_text
-		puts doc.at("robbery").inner_text
-puts 		doc.at("thieves").inner_text
-puts 		doc.at("kidnapping").inner_text
-puts 		doc.at("extortion").inner_text
-puts 		doc.at("swindler").inner_text
-puts 		doc.at("others").inner_text
-puts 		doc.at("total").inner_text
-		
-		
+		{:delegacy_name=>doc.at('delegacy/delegacy').inner_text,
+			:thieves => doc.at('thieves').inner_text,
+			:robery => doc.at('robbery').inner_text,
+			:kidnapping => doc.at('kidnapping').inner_text,
+			:extortion => doc.at('extortion').inner_text,
+			:swindler => doc.at('swindler').inner_text,
+			:other => doc.at('others').inner_text,									
+			:total => doc.at('total').inner_text}				
 	end
 	
 end
